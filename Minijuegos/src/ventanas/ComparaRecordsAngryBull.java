b21d4371d1ec46170fb19b9eae262835025018a9
aitor <aitor@192.168.1.35>
2014-12-12 20:39:07 +0100

    Segundo commit

---
+++ Minijuegos/src/ventanas/ComparaRecordsAngryBull.java
@@ -0,0 +1,74 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComparaRecordsAngryBull {
+
+	JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ComparaRecordsAngryBull window = new ComparaRecordsAngryBull();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ComparaRecordsAngryBull() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.CYAN);
+		frame.setBackground(UIManager.getColor("Button.background"));
+		frame.getContentPane().setForeground(Color.CYAN);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblComparacion = new JLabel("COMPARACI\u00D3N DE RECORDS ANGRY BULL");
+		lblComparacion.setFont(new Font("Segoe UI Black", Font.PLAIN, 15));
+		lblComparacion.setHorizontalAlignment(SwingConstants.CENTER);
+		lblComparacion.setBounds(58, 11, 310, 29);
+		frame.getContentPane().add(lblComparacion);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.setBounds(170, 227, 89, 23);
+		frame.getContentPane().add(btnAtras);
+		btnAtras.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Salir");
+			EstadiJugadorAngryBull a=new EstadiJugadorAngryBull();
+			a.frame.setVisible(true);
+		}
+		});
+		}
+}
---
+++ Minijuegos/src/ventanas/EstadiMundialPepeFly.java
@@ -0,0 +1,78 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import clases.UsuariosBD;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+
+public class EstadiMundialPepeFly{
+
+	JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EstadiMundialPepeFly window = new EstadiMundialPepeFly();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * @wbp.parser.entryPoint
+	 */
+	public EstadiMundialPepeFly() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.CYAN);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRankingMundial = new JLabel("TOP 10 MUNDIAL PEPE FLY");
+		lblRankingMundial.setFont(new Font("Stencil", Font.PLAIN, 18));
+		lblRankingMundial.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRankingMundial.setBounds(64, 28, 313, 29);
+		frame.getContentPane().add(lblRankingMundial);
+		
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.setBounds(170, 227, 89, 23);
+		frame.getContentPane().add(btnAtras);
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Salir");
+				ElegirEstadPepeFly a=new ElegirEstadPepeFly();
+				a.frame.setVisible(true);
+			}
+		});
+		
+		/*JLabel lblRecord = new JLabel(UsuariosBD.recordMundialPartida(1));
+		lblRecord.setBackground(new Color(0, 255, 0));
+		lblRecord.setBounds(165, 85, 179, 14);
+		frame.getContentPane().add(lblRecord);*/
+		
+	}
+}
